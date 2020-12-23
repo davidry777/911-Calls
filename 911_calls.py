7f745b6badc9e7c0c46f4acbc78d7df679cdac3d
davidry777 <dryan0319@gmail.com>
2020-12-23 14:38:13 -0800

    Created countplots for Reason and Day of Week columns

--- 911_calls.py
+++ 911_calls.py
@@ -22,3 +22,23 @@ print(df['title'].nunique())
 df['Reason'] = df['title'].apply(lambda title: title.split(':')[0])
 # Analyzing most common reasons for 911 call
 print(df['Reason'].value_counts())
+
+# Creating a countplot of 911 calls by Reason
+sns.countplot(x='Reason', data=df)
+plt.show()
+
+# Converting timeStamp column to DateTime objects
+df['timeStamp'] = pd.to_datetime(df['timeStamp'])
+
+# Creating new columns Hour, Month, and Day of  Week
+df['Hour'] = df['timeStamp'].apply(lambda time: time.hour)
+df['Month'] = df['timeStamp'].apply(lambda time: time.month)
+df['Day of Week'] = df['timeStamp'].apply(lambda time: time.dayofweek)
+
+# Mapping actual string days to Day of Week column
+dmap = {0:'Mon',1:'Tue',2:'Wed',3:'Thu',4:'Fri',5:'Sat',6:'Sun'}
+df['Day of Week'] = df['Day of Week'].map(dmap)
+
+# Creating a countplot for the Day of Week column with hue based on Reason
+sns.countplot(x='Day of Week', data=df, hue='Reason')
+plt.show()
